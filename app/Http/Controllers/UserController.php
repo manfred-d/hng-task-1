93d65d4319bcad2130aeeee978e408a58ab56db7
Manfred <manfredkyp@gmail.com>
2022-10-28 13:17:00 +0300

    First commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(request $request){
+        return response()->json([
+            'SlackUsername' => "Manfred",
+            'backend' => true,
+            'age' => 23,
+            'bio' => "I am a student, Full-Stack Dev. I am glad to be here to learn",
+        ]);
+    }
+}
