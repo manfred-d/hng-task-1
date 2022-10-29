0ceb124c1f6f65dead958cc07245e69d876d2f44
Manfred <manfredkyp@gmail.com>
2022-10-30 00:00:29 +0300

    corrected the username

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,7 @@ class UserController extends Controller
 {
     public function index(request $request){
         return response()->json([
-            'SlackUsername' => "Manfred",
+            'slackUsernameslackUsername' => "Manfred",
             'backend' => true,
             'age' => 23,
             'bio' => "I am a student, Full-Stack Dev. I am glad to be here to learn",
